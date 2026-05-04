0b7c47726a7b3049f6d2e52c5f7a25d20e33dea7
HRADigital <github@hradigital.com>
2026-05-04 21:07:50 +0100

    Fix CJS bundle by using explicit .cjs/.mjs extensions (v0.1.1)

--- rollup.config.js
+++ rollup.config.js
@@ -6,8 +6,8 @@ const banner = `/*! @hradigital/smartforms | GPL-2.0-or-later */`;
 export default {
     input: 'src/index.js',
     output: [
-        { file: 'dist/smartforms.esm.js', format: 'esm', banner, sourcemap: true },
-        { file: 'dist/smartforms.cjs.js', format: 'cjs', banner, sourcemap: true, exports: 'named' },
+        { file: 'dist/smartforms.mjs', format: 'esm', banner, sourcemap: true },
+        { file: 'dist/smartforms.cjs', format: 'cjs', banner, sourcemap: true, exports: 'named' },
         {
             file: 'dist/smartforms.umd.js',
             format: 'umd',
