f1651b2741bbdb7b79560d819bfb75ba01eba5d6
HRADigital <github@hradigital.com>
2026-05-08 11:05:03 +0100

    Add EmailInput type extending TextInput with email-format validation

---
+++ src/form/input/EmailInput.js
@@ -0,0 +1,27 @@
+import TextInput from './TextInput';
+
+/**
+ * Form Email Input handling class.
+ *
+ * @package   SmartForms\Assets
+ */
+class EmailInput extends TextInput {
+    /**
+     * Validates if the input is currently holding an illegal value.
+     *
+     * @return {boolean}
+     */
+    isIllegal() {
+        if (this._required && this._value.trim().length === 0) {
+            return true;
+        }
+
+        if (this._value.trim().length > 0 && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this._value.trim())) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+export default EmailInput;
