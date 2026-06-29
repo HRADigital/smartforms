2df2c1dbf66bccb68ec2fab57e552f34fbe3197d
HRADigital <github@hradigital.com>
2026-06-29 17:17:08 +0100

    Silence intentional no-console warnings in logger.js debug output

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,7 @@
 let debug = false;
 
 export function log(...args) {
+    // eslint-disable-next-line no-console -- intentional debug logger output
     if (debug) console.log(...args);
 }
 
@@ -10,6 +11,7 @@ export function warn(...args) {
 
 export function toggleDebug(v) {
     debug = v !== undefined ? Boolean(v) : !debug;
+    // eslint-disable-next-line no-console -- intentional debug-state notice
     console.log('[SmartForms] debug', debug);
     return debug;
 }
