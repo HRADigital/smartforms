2215945cb8faf2faae0c8210422e721f10fafd55
HRADigital <github@hradigital.com>
2026-07-13 18:08:00 +0100

    Add character limit enforcement and counter guide to TextAreaInput

--- src/form/input/TextAreaInput.js
+++ src/form/input/TextAreaInput.js
@@ -20,7 +20,14 @@ class TextAreaInput extends BaseInput {
         this._input = fieldset.querySelector('textarea');
         this._name = this._input.getAttribute('name');
         this._value = this._input.value.trim();
+        this._limit = this._input.getAttribute('data-limit');
+        const showGuide = this._input.getAttribute('data-guide');
+        this._guide = null;
         this._initial = this._value;
+
+        // Loads and configures character limit processing.
+        this.loadLimit(this._input);
+        this.loadGuide(showGuide !== null);
 
         // Sets up the control events.
         this._input.addEventListener('change', (e) => this.onChange(e));
@@ -31,21 +38,104 @@ class TextAreaInput extends BaseInput {
     }
 
     /**
-     * Event handler for when the SelectInput changes value.
+     * Loads the character limit configuration.
+     *
+     * @param {object} element
+     */
+    loadLimit(element) {
+        if (this._limit) {
+            this._limit = parseInt(this._limit);
+            element.addEventListener('keypress', (e) => this.onKeyPress(e));
+        }
+    }
+
+    /**
+     * Loads the character limit guiding counter.
+     *
+     * @param {boolean} show
+     */
+    loadGuide(show) {
+        // Checks if the character limit guide counter should be rendered.
+        if (!show) {
+            return;
+        }
+
+        // Loads the character guide to the webcontrol.
+        const parent = document.createElement('SPAN');
+        parent.className = 'smartguide';
+
+        this._guide = document.createElement('SPAN');
+        const cText = document.createTextNode(this._input.value.length);
+        this._guide.appendChild(cText);
+
+        const total = document.createElement('SPAN');
+        const tText = document.createTextNode(this._limit);
+        total.appendChild(tText);
+
+        const space = document.createElement('SPAN');
+        const sText = document.createTextNode('/');
+        space.appendChild(sText);
+
+        parent.appendChild(this._guide);
+        parent.appendChild(space);
+        parent.appendChild(total);
+
+        this._input.parentNode.insertBefore(parent, this._input);
+    }
+
+    /**
+     * Processes the character limit counter, after text has been introduced in the Textarea.
+     *
+     * @param {int} length
+     */
+    processGuide(length) {
+        // Updates the character's counter.
+        const textnode = document.createTextNode(length);
+        this._guide.replaceChild(textnode, this._guide.childNodes[0]);
+
+        // Validates if a warning should be processed, if the character limit is reaching the limit.
+        if (length / this._limit > 0.9) {
+            this._guide.className = 'warning';
+        } else {
+            this._guide.className = '';
+        }
+    }
+
+    /**
+     * Event handler for when the TextAreaInput changes value (onBlur).
      *
      * @param {event} e
      */
     onChange(e) {
         this.processChange(e.target.value.trim());
+
+        if (this._guide !== null) {
+            this.processGuide(e.target.value.length);
+        }
     }
 
     /**
-     * Event handler for when the TextInput changes value, while editing it.
+     * Event handler for when the TextAreaInput changes value, while editing it.
      *
      * @param {event} e
      */
     onKey(e) {
         this.processChange(e.target.value.trim());
+
+        if (this._guide !== null) {
+            this.processGuide(e.target.value.length);
+        }
+    }
+
+    /**
+     * Event handler blocking input past the character limit.
+     *
+     * @param {event} e
+     */
+    onKeyPress(e) {
+        if (e.target.value.length >= this._limit) {
+            e.preventDefault();
+        }
     }
 
     /**
